153eeedf4dcdbefd6d43e0a0e48ce4d24f4500cd
nazmul-2020 <nazmulcpi2012@gmail.com>
2024-05-14 16:32:50 +0600

    Video Feature add

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import videosReducer from "../features/videos/videosSlice";
+import tagsReducer from "../features/tags/TagsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    videos: videosReducer,
+    tags: tagsReducer,
   },
 });
